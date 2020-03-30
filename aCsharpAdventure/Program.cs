5f2df03014cb993348f2bcaa7935899a924a3da7
Marcus Johnsson <mr.marcus.marega@gmail.com>
2020-03-30 17:43:37 +0200

    loops and arrays

--- aCsharpAdventure/Program.cs
+++ aCsharpAdventure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace aCsharpAdventure
 {
@@ -19,12 +20,14 @@ namespace aCsharpAdventure
                 Console.WriteLine("[6] - Guessing Game");
                 Console.WriteLine("[7] - Add stuff");
                 Console.WriteLine("[8] - Add stuff some more");
+                Console.WriteLine("[9] - Add 5 strings to a list and print them");
+                Console.WriteLine("[10] - Sums a specified set of numbers provided");
                 Console.WriteLine("Type EXIT to close the application");
 
                 string opt = Console.ReadLine();
 
                 // Exit app
-                if(opt == "EXIT")
+                if (opt == "EXIT")
                 {
                     exit = true;
                 }
@@ -35,7 +38,7 @@ namespace aCsharpAdventure
                 {
                     Console.WriteLine("Thats not a valid option");
                 }
-                    
+
 
                 switch (menuOption)
                 {
@@ -79,6 +82,16 @@ namespace aCsharpAdventure
                         arraySum();
                         break;
 
+                    case 9:
+
+                        stringyList();
+                        break;
+
+                    case 10:
+
+                        loopLoop();
+                        break;
+
                     default:
 
                         break;
@@ -111,7 +124,7 @@ namespace aCsharpAdventure
             Console.WriteLine("Done! Hot dang that be some fast counting...");
             Console.WriteLine("Ye want me to do it again but slower? Y / N");
             string answer = Console.ReadLine();
-            
+
             if (answer == "Y")
             {
                 int milliSecs = 100;
@@ -143,7 +156,7 @@ namespace aCsharpAdventure
             int yearBorn = int.Parse(Console.ReadLine());
             int age = DateTime.Now.Year - yearBorn;
 
-            if(age < 0)
+            if (age < 0)
             {
                 int yearsIntoFuture = yearBorn - DateTime.Now.Year;
                 Console.Write("Wooooow! You are from " + yearsIntoFuture + " years into the frikkin future!");
@@ -151,27 +164,27 @@ namespace aCsharpAdventure
             }
             else
             {
-                    if (age > 125)
-                    {
-                        Console.WriteLine("So you are " + (DateTime.Now.Year - yearBorn) + " years old huh?. Im a computer, not a fool.");
-                        Console.WriteLine("One more time, truthfully.");
-                        yourAge(); // Recursive function call to get the truth!
-                    }
-                    else if (age <= 120 && age > 70)
-                    {
-                        Console.WriteLine("You are " + (DateTime.Now.Year - yearBorn) + " years old...");
-                        System.Threading.Thread.Sleep(3000);
-                        Console.WriteLine(".. and soon dead.");
-                        System.Threading.Thread.Sleep(1000);
-                        Console.WriteLine("=(");
-                        System.Threading.Thread.Sleep(3000);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("You are " + (DateTime.Now.Year - yearBorn) + " years old.");
-                    }
+                if (age > 125)
+                {
+                    Console.WriteLine("So you are " + (DateTime.Now.Year - yearBorn) + " years old huh?. Im a computer, not a fool.");
+                    Console.WriteLine("One more time, truthfully.");
+                    yourAge(); // Recursive function call to get the truth!
                 }
+                else if (age <= 120 && age > 70)
+                {
+                    Console.WriteLine("You are " + (DateTime.Now.Year - yearBorn) + " years old...");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine(".. and soon dead.");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine("=(");
+                    System.Threading.Thread.Sleep(3000);
+
+                }
+                else
+                {
+                    Console.WriteLine("You are " + (DateTime.Now.Year - yearBorn) + " years old.");
+                }
+            }
 
         }
         static void tellJoke()
@@ -180,7 +193,7 @@ namespace aCsharpAdventure
             Console.WriteLine("Ye wanna hear a joke? Y / N");
             string v = Console.ReadLine();
             string answer = v;
-            if(answer == "Y")
+            if (answer == "Y")
             {
                 Console.WriteLine(" ");
                 Console.WriteLine("I mainly only program in C# and well I can never find any good jokes, about this language.");
@@ -192,7 +205,7 @@ namespace aCsharpAdventure
                 Console.WriteLine(".");
                 Console.WriteLine("Got it? Y / N:");
                 string response = Console.ReadLine();
-                if(response == "Y")
+                if (response == "Y")
                 {
                     Console.WriteLine("Your a genius!");
                 }
@@ -205,7 +218,7 @@ namespace aCsharpAdventure
             {
                 Console.WriteLine("No? You must not be to sharp...");
             }
-     
+
         }
         static void addOrMultiply()
         {
@@ -245,7 +258,7 @@ namespace aCsharpAdventure
 
         }
 
-       
+
         static void crashAndBurn()
         {
             // Example of TryParse(String, Int32)
@@ -332,10 +345,10 @@ namespace aCsharpAdventure
             string second = Console.ReadLine();
             string third = Console.ReadLine();
             string fourth = Console.ReadLine();
-           
+
             Console.WriteLine(addStuff(first, second, third, fourth));
         }
-        // adds a bunch of strings together
+        // Populate an array with integer and sum up the elements. 
         static void arraySum()
         {
             const int MAX_SIZE = 5;
@@ -344,23 +357,67 @@ namespace aCsharpAdventure
             Console.WriteLine("Provide 5 numbers and will sum them up");
             while (i < MAX_SIZE)
             {
-                nums [i] = Convert.ToInt32(Console.ReadLine());
+                nums[i] = Convert.ToInt32(Console.ReadLine());
                 i++;
             }
             int sum = 0;
-            i = MAX_SIZE -1; // Correcting i as delimiter since i was set to surpas MAX_SIZE by ++ in prior while-loop. 
+            i = MAX_SIZE - 1; // Correcting i as delimiter since i was set to surpas MAX_SIZE by ++ in prior while-loop. 
             while (i >= 0)
-            {   
-                int atIndex = nums[i];
-                int displayedSum = Convert.ToInt32(sum + atIndex);
-                Console.WriteLine(sum + " + " + atIndex + " = " + displayedSum);
-                sum = sum + atIndex;
+            {
+                int valueAtIndex = nums[i];
+                int displayedSum = Convert.ToInt32(sum + valueAtIndex);
+                Console.WriteLine(sum + " + " + valueAtIndex + " = " + displayedSum);
+                sum = sum + valueAtIndex;
                 i = i - 1;
             }
             Console.WriteLine("And that adds up to a total of " + sum);
 
         }
-
+        // Adds 5 strings to list and prints them out once the list is full. 
+        static void stringyList()
+        {
+            const int MAX_SIZE = 5;
+            int i = 0; // Counter
+            List<string> stringList = new List<string> { };
+            Console.WriteLine("Add 5 things to this list:");
+            while (i < MAX_SIZE)
+            {
+                stringList.Add(Console.ReadLine());
+                i++;
+                if (i == MAX_SIZE)
+                {
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine("Good Job! Now lets print out the list:");
+                    i = 0;
+                    while (i < MAX_SIZE)
+                    {
+                        System.Threading.Thread.Sleep(800);
+                        Console.WriteLine(stringList[i]);
+                        i++;
+                    }
+                    Console.WriteLine("Tadaaa! That is a list folks! ");
+                    System.Threading.Thread.Sleep(3000);
+                }
+            }
+        }
+        static void loopLoop()
+        {
+            // create an aray with the size determined by the user
+            Console.WriteLine("State a set of numbers you want to sum up as an integer:");
+            double[] arr = new double[Convert.ToInt32(Console.ReadLine())];
+            Console.WriteLine("Provide the numbers:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("Total is: " + sum);    
+        }
     }
 
 }
